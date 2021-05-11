e8fd1946b6d6816515d59ed41fae9efa18e408a9
lfoever <1521807627@qq.com>
2021-05-11 19:38:40 +0800

    添加了 nav

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <div class="box"></div>
     <div class="boxw"></div>
+    <Nav />
     <Tap />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
 import Tap from "./components/Tap.vue";
 
 export default {
@@ -18,6 +20,8 @@ export default {
     HelloWorld,
 
     Tap,
+
+    Nav,
   },
 };
 </script>
---
+++ src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <div><h2>ni好</h2></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
